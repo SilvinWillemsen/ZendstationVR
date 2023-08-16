86a251afaf3374d6b6c452cfc39378a366160663
20225896 <s.willemsen@tue.nl>
2023-08-16 17:31:12 +0200

    Sharepoint link

--- Assets/TeleportController.cs
+++ Assets/TeleportController.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 using UnityEngine.Events;
 
 // https://www.youtube.com/watch?v=wGvh7Suo1h4
-
+//https://tuenl-my.sharepoint.com/:v:/r/personal/s_willemsen_tue_nl/Documents/ZendstationVR%20-%20SampleScene%20-%20Windows,%20Mac,%20Linux%20-%20Unity%202021.3.21f1%20Personal%20_DX11_%202023-08-16%2017-23-04.mp4?csf=1&web=1&e=Tur3Sy
 public class TeleportController : MonoBehaviour
 {
     public GameObject baseControllerGameObject;
