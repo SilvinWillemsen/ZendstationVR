bd553b96ccb03e2d007c57ece07ec5a7aa7e802a
Silvin Willemsen <silvinwillemsen@gmail.com>
2023-09-15 15:16:14 +0200

    Figured out that the ZenderzaalVRExport_01 gameobject was scaled by 100, so scaled it down. Also added sound and Steam Audio. Managed to change materials using A and B buttons.

---
+++ Assets/ChangeMaterialController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+using TMPro;
+public class ChangeMaterialController : MonoBehaviour
+{
+    public GameObject ZenderZaalA;
+    public GameObject ZenderZaalB;
+
+    public InputActionReference AbuttonClick;
+    public InputActionReference BbuttonClick;
+
+    [Space]
+    public UnityEvent onAButtonClicked;
+    public UnityEvent onBButtonClicked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        AbuttonClick.action.performed += ClickAButton;
+        BbuttonClick.action.performed += ClickBButton;
+
+        // Select material A to begin with
+        //SelectMaterialA();
+    }
+    private void ClickAButton(InputAction.CallbackContext obj) => onAButtonClicked.Invoke();
+    private void ClickBButton(InputAction.CallbackContext obj) => onBButtonClicked.Invoke();
+
+    public void SelectMaterialA()
+    {
+        ZenderZaalA.SetActive (true);
+        ZenderZaalB.SetActive (false);
+    }
+    public void SelectMaterialB()
+    {
+        ZenderZaalA.SetActive (false);
+        ZenderZaalB.SetActive (true);
+    }
+
+}
